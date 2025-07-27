e3fc4e761120dac634009064db7faa25dd7455a5
Febrian Amir <febrianamir100@gmail.com>
2025-07-27 17:59:42 +0800

    feat(editor): ctrl/command+s trigger to save, tab filename

--- frontend/src/components/Editor.svelte
+++ frontend/src/components/Editor.svelte
@@ -4,6 +4,7 @@
   import { onMount, onDestroy } from 'svelte'
   import { get } from 'svelte/store'
 
+  export let filename = 'Untitled'
   export let value = '# Hello\nThis is a markdown note.'
   export let placeholder = 'Write your markdown here...'
   export let rows = 35
@@ -13,30 +14,12 @@
   let errorMessage = ''
   let successMessage = ''
 
-  onMount(() => {
-    unsubOpenedFile = openedFile.subscribe(async path => {
-      if (path && path.trim() !== '') {
-        try {
-          const result = await OpenFile(path)
-          value = result
-        } catch (err) {
-          console.error('Error fetching file:', err)
-          value = ''
-        }
-      }
-    })
-  })
-
-  onDestroy(() => {
-    unsubOpenedFile?.()
-  })
-
-  const handleInput = event => {
-    value = event.target.value
+  const handleInput = e => {
+    value = e.target.value
     dispatchEvent(new CustomEvent('input', { detail: value }))
   }
 
-  const saveFile = async () => {
+  const handleSave = async () => {
     errorMessage = ''
     successMessage = ''
     try {
@@ -47,9 +30,46 @@
       errorMessage = err.message || 'Failed to save directory.'
     }
   }
+
+  const onKeyDown = e => {
+    if ((e.ctrlKey || e.metaKey) && e.key === 's') {
+      // Ctrl+S or Command+S for MacOS
+      e.preventDefault()
+      handleSave()
+    }
+  }
+
+  onMount(() => {
+    unsubOpenedFile = openedFile.subscribe(async path => {
+      if (path && path.trim() !== '') {
+        // Open file
+        try {
+          const result = await OpenFile(path)
+          value = result
+        } catch (err) {
+          console.error('Error fetching file:', err)
+          value = ''
+        }
+
+        // Set tab filename
+        filename = path.split('/').pop()
+      }
+    })
+    window.addEventListener('keydown', onKeyDown)
+  })
+
+  onDestroy(() => {
+    unsubOpenedFile?.()
+    window.removeEventListener('keydown', onKeyDown)
+  })
 </script>
 
 <div class="editor">
+  <div class="editor-tab">
+    <div class="editor-tab-item">
+      {filename}
+    </div>
+  </div>
   <textarea
     class="editor-textarea"
     bind:value={value}
@@ -58,18 +78,46 @@
     placeholder={placeholder}
     on:input={handleInput}
   ></textarea>
-  <button on:click={saveFile}>Save</button>
 </div>
 
 <style>
   .editor {
+    display: flex;
+    flex-direction: column;
     height: 100vh;
+    overflow: hidden;
+  }
+
+  .editor-tab {
+    background-color: #282828;
+    border-bottom: 2px solid #1e1e1e;
+  }
+
+  .editor-tab-item {
+    display: inline-block;
+    position: relative;
+    cursor: pointer;
+    font-size: 0.85rem;
+    padding: 0.5rem 0.75rem;
+    color: #9d9d9d;
+  }
+
+  .editor-tab-item::after {
+    content: '';
+    position: absolute;
+    bottom: -1px; /* Align with .tab's black border */
+    width: 100%;
+    left: 0;
+    bottom: 0;
+    height: 2px;
+    background-color: #6da96f;
+    border-radius: 1px;
   }
 
   .editor-textarea {
     all: unset; /* Resets most browser default styles */
-    width: 100%;
-    height: 100%;
+    flex: 1;
+    overflow: auto;
     resize: none;
     font-size: 0.85rem;
     font-family: 'JetBrains Mono', monospace;
