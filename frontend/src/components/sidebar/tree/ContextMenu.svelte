7b7879e737747a68321020d117420b2ad9d04129
Febrian Amir <febrianamir100@gmail.com>
2025-08-06 11:32:50 +0800

    refactor(tree): separate ContextMenu component

---
+++ frontend/src/components/sidebar/tree/ContextMenu.svelte
@@ -0,0 +1,60 @@
+<script>
+  export let contextMenuX = 0
+  export let contextMenuY = 0
+
+  export let handleOpenCreateFileInput = () => {}
+  export let handleOpenCreateDirectoryInput = () => {}
+</script>
+
+<div class="tree-context-menu" style="top:{contextMenuY}px; left:{contextMenuX}px;">
+  <div
+    role="button"
+    tabindex="0"
+    class="tree-context-item"
+    on:click={handleOpenCreateFileInput}
+    on:keydown={(e) => {
+      if (e.key === 'Enter' || e.key === ' ') {
+        e.preventDefault()
+        handleOpenCreateFileInput()
+      }
+    }}
+  >
+    New File
+  </div>
+  <div
+    role="button"
+    tabindex="0"
+    class="tree-context-item"
+    on:click={handleOpenCreateDirectoryInput}
+    on:keydown={(e) => {
+      if (e.key === 'Enter' || e.key === ' ') {
+        e.preventDefault()
+        handleOpenCreateDirectoryInput()
+      }
+    }}
+  >
+    New Directory
+  </div>
+</div>
+
+<style>
+  .tree-context-menu {
+    position: fixed;
+    z-index: 1000;
+    background: #282828;
+    border: 1px solid #323232;
+    border-radius: 2px;
+    font-size: 0.8rem;
+    padding: 0.2rem 0;
+  }
+
+  .tree-context-item {
+    padding: 0.35rem 1.2rem;
+    cursor: pointer;
+  }
+
+  .tree-context-item:hover {
+    color: #282828;
+    background-color: #c9e6c1;
+  }
+</style>
--- frontend/src/components/sidebar/tree/Tree.svelte
+++ frontend/src/components/sidebar/tree/Tree.svelte
@@ -1,5 +1,6 @@
 <script>
   import TreeNode from './TreeNode.svelte'
+  import ContextMenu from './ContextMenu.svelte'
 
   import { GetNodeTree } from '../../../../wailsjs/go/usecase/Usecase.js'
   import { rootPath } from '../../../stores/global.js'
@@ -184,36 +185,12 @@
   {/if}
 
   {#if showContextMenu}
-    <div class="tree-context-menu" style="top:{contextMenuY}px; left:{contextMenuX}px;">
-      <div
-        role="button"
-        tabindex="0"
-        class="tree-context-item"
-        on:click={handleOpenCreateFileInput}
-        on:keydown={(e) => {
-          if (e.key === 'Enter' || e.key === ' ') {
-            e.preventDefault()
-            handleOpenCreateFileInput()
-          }
-        }}
-      >
-        New File
-      </div>
-      <div
-        role="button"
-        tabindex="0"
-        class="tree-context-item"
-        on:click={handleOpenCreateDirectoryInput}
-        on:keydown={(e) => {
-          if (e.key === 'Enter' || e.key === ' ') {
-            e.preventDefault()
-            handleOpenCreateDirectoryInput()
-          }
-        }}
-      >
-        New Directory
-      </div>
-    </div>
+    <ContextMenu
+      contextMenuX={contextMenuX}
+      contextMenuY={contextMenuY}
+      handleOpenCreateFileInput={handleOpenCreateFileInput}
+      handleOpenCreateDirectoryInput={handleOpenCreateDirectoryInput}
+    />
   {/if}
 </div>
 
@@ -223,25 +200,5 @@
     overflow-x: hidden;
     overflow-y: scroll;
     box-sizing: border-box;
-  }
-
-  .tree-context-menu {
-    position: fixed;
-    z-index: 1000;
-    background: #282828;
-    border: 1px solid #323232;
-    border-radius: 2px;
-    font-size: 0.8rem;
-    padding: 0.2rem 0;
-  }
-
-  .tree-context-item {
-    padding: 0.35rem 1.2rem;
-    cursor: pointer;
-  }
-
-  .tree-context-item:hover {
-    color: #282828;
-    background-color: #c9e6c1;
   }
 </style>
