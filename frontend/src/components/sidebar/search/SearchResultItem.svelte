c92cf41be4b1349d30d8da4d4d370a62ce1e17dc
Febrian Amir <febrianamir100@gmail.com>
2025-08-09 21:02:25 +0800

    feat(search): add tooltip to search result filename
    Tooltip now appears after delay on search result filenames, displaying the file's full path.

--- frontend/src/components/sidebar/search/SearchResultItem.svelte
+++ frontend/src/components/sidebar/search/SearchResultItem.svelte
@@ -1,26 +1,74 @@
 <script>
   import { ChevronDown, ChevronRight } from 'lucide-svelte'
   import { handleEnter } from '../../../../src/lib/utils'
+  import { onDestroy, onMount } from 'svelte'
 
   export let filename
   export let path
   export let fileMatches
 
+  let clientX = 0
+  let clientY = 0
+
   let isExpanded = true
+  let isShowTooltip = false
+  let tooltipX = 0
+  let tooltipY = 0
+  let tooltipShowTimer
 
   function toggleExpand() {
     isExpanded = !isExpanded
   }
+
+  function openTooltip(e) {
+    tooltipX = clientX + 10
+    tooltipY = clientY + 10
+    isShowTooltip = true
+  }
+
+  function closeTooltip(e) {
+    isShowTooltip = false
+  }
+
+  function onMouseMove(e) {
+    clientX = e.clientX
+    clientY = e.clientY
+  }
+
+  onMount(() => {
+    window.addEventListener('mousemove', onMouseMove)
+  })
+
+  onDestroy(() => {
+    window.removeEventListener('mousemove', onMouseMove)
+  })
 </script>
 
 <div class="search-result-item">
   <div
     class="search-result-file"
-    on:click={toggleExpand}
+    on:click={() => {
+      clearTimeout(tooltipShowTimer)
+      toggleExpand()
+    }}
     on:keydown={(e) => {
+      clearTimeout(tooltipShowTimer)
       handleEnter(e, toggleExpand)
     }}
+    on:mouseenter={(e) => {
+      clearTimeout(tooltipShowTimer)
+      tooltipShowTimer = setTimeout(() => {
+        openTooltip(e)
+      }, 1000)
+    }}
+    on:mouseleave={() => {
+      clearTimeout(tooltipShowTimer)
+      closeTooltip()
+    }}
   >
+    {#if isShowTooltip}
+      <div class="search-tooltip" style="top:{tooltipY}px; left:{tooltipX}px;">{path}</div>
+    {/if}
     <div class="search-result-file-icon">
       {#if isExpanded}
         <ChevronDown size={16} strokeWidth={3} />
@@ -40,6 +88,10 @@
 </div>
 
 <style>
+  .search-result-item {
+    position: relative;
+  }
+
   .search-result-file,
   .search-result-match {
     user-select: none;
@@ -53,6 +105,7 @@
     gap: 0.1rem;
     cursor: default;
     font-weight: 600;
+    padding-top: 0.25rem;
   }
 
   .search-result-matches {
@@ -66,5 +119,21 @@
   .search-result-match {
     cursor: pointer;
     padding-left: 0.75rem;
+  }
+
+  .search-tooltip {
+    position: fixed;
+    z-index: 1000;
+    top: 0;
+    left: 0;
+    background: #181818;
+    color: #9d9d9d;
+    padding: 4px 8px;
+    font-weight: 400;
+    font-size: 12px;
+    white-space: nowrap;
+    /* border-radius: 4px; */
+    border: 1px solid #9d9d9d;
+    transform: translateY(-4px);
   }
 </style>
