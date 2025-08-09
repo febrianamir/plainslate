287f52ea92ca759468ea9b6f96731c7503a63bfd
Febrian Amir <febrianamir100@gmail.com>
2025-08-09 11:58:56 +0800

    refactor: rename backend files name

--- backend/model/node.go
+++ backend/model/node.go
@@ -3,12 +3,12 @@ package model
 type Node struct {
 	Name     string  `json:"name"`
 	Path     string  `json:"path"`
-	Type     string  `json:"type"` // Node Type
+	Type     string  `json:"type"` // Node Types
 	Children []*Node `json:"children,omitempty"`
 	IsRoot   bool    `json:"is_root"`
 }
 
-// Node Type
+// Node Types
 const (
 	NodeTypeFile      = "file"
 	NodeTypeDirectory = "directory"
